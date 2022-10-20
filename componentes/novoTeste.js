96f8c1210a3460583b98573e784e043a5a441888
Sabrola <70954761+Sabrola@users.noreply.github.com>
2022-10-20 09:06:34 -0300

    Add files via upload

---
+++ componentes/novoTeste.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+/* function ouTeste() {
+    return <p> Este é outro teste.</p>
+} */
+
+const ouTeste = () => <p>Este é outro teste. </p>
+
+export default ouTeste
