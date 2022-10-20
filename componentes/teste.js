368b723ed95ae19b72c300b9b4d027d88b084e3e
Sabrola <70954761+Sabrola@users.noreply.github.com>
2022-10-20 09:06:26 -0300

    Create teste.js

---
+++ componentes/teste.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+function Teste() {
+    /* Um componente React ode retornar apenas UM elemento. Caso sejá
+    necessário retornar mais de um elemento, pode-se agrupa-los dentro de
+    um pseudoElemento chamado Fragment, representado por '<>' e '</> */
+    return (
+    <>
+        <h1> Página Inicial do Projeto </h1>
+        <p> Este é um componente customizado criado em um prejeto React</p>
+    </>
+    )
+}
+
+export default Teste 
